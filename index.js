7a8bd05e45d9f47d5536e5f7f93d5de502df23b6
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-08-23 21:43:51 +0400

    feat(index): Added useAuth hook

---
+++ index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ProvideAuth } from "./hooks/useAuth.hook";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ProvideAuth>
+      <Router>
+        <App />
+      </Router>
+    </ProvideAuth>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
