a443b66893dfc79b33287599e004d80b0f9c8a2d
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-08-24 18:23:37 +0400

    Small changes

--- src/hooks/useAuth.hook.js
+++ src/hooks/useAuth.hook.js
@@ -4,16 +4,22 @@ import { addUser } from "../services/user.services";
 
 const authContext = createContext();
 
+// Provider component that wraps your app and makes auth object ...
+// ... available to any child component that calls useAuth().
 export function ProvideAuth({ children }) {
   const auth = useProvideAuth();
   return <authContext.Provider value={auth}>{children}</authContext.Provider>;
 }
+// Hook for child components to get the auth object ...
+// ... and re-render when it changes.
 export const useAuth = () => {
   return useContext(authContext);
 };
-
+// Provider hook that creates auth object and handles state
 function useProvideAuth() {
   const [user, setUser] = useState(null);
+  // Wrap any Firebase methods we want to use making sure ...
+  // ... to save the user to state.
   const signin = (email, password) => {
     return firebase
       .auth()
@@ -67,6 +73,11 @@ function useProvideAuth() {
       });
   };
 
+  // Subscribe to user on mount
+  // Because this sets state in the callback it will cause any ...
+  // ... component that utilizes this hook to re-render with the ...
+  // ... latest auth object.
+
   useEffect(() => {
     const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
       if (user) {
@@ -75,9 +86,11 @@ function useProvideAuth() {
         setUser(false);
       }
     });
+    // Cleanup subscription on unmount
     return () => unsubscribe();
   }, []);
 
+  // Return the user object and auth methods
   return {
     user,
     signin,
--- src/libs/firebase.libs.js
+++ src/libs/firebase.libs.js
@@ -6,7 +6,7 @@ export default firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
-  // appID: process.env.REACT_APP_APP_ID,
+  appID: process.env.REACT_APP_APP_ID,
   databaseURL: process.env.REACT_APP_DB_URL,
   messagingSenderId: process.env.REACT_APP_MS,
   storageBucket: process.env.REACT_APP_SB,
